20dab18f49db710b696a619040b633d821b3bc82
ITOL <1104026790@qq.com>
2019-07-20 18:23:22 +0800

    合并版本

--- pages/user/myincome/myincome.js
+++ pages/user/myincome/myincome.js
@@ -2,72 +2,99 @@
 const app = getApp()
 var fileData = require("../../../utils/data.js");
 var commonData = require("../../../utils/util.js");
-
+const date = new Date() 
 Page({
 
   /**
    * 页面的初始数据
    */
-	data: {
-		clubIncomeData: null,
-		clubIncomeSum: {},
-		date: '2019-04-14'
-	},
+  data: {
+    incomeDate: fileData.getIncomeData(),
+    date: commonData.formatTime(date)
+  },
 
-	bindDateChange: function (e) {
-		console.log('picker发送选择改变，携带值为', e.detail.value)
-		this.setData({
-			date: e.detail.value
-		})
-		var url_tmp = commonData.getListConfig().url_test;
-		var _this = this;
-		wx.request({
-			url: url_tmp + '/club/qryLessReg',
-			data: {
-				club_id: app.globalData.user_id,
-				reg_date: e.detail.value,
-				// status: 1
-			},
-			success(res) {
-				console.log(res.data)
-				_this.setData({
-					clubIncomeData: res.data
-				})
-
-			}
-		})
-	},
+  bindDateChange: function (e) {
+    var _this = this
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    _this.setData({
+      date: e.detail.value
+    })
+    var url_tmp = commonData.getListConfig().url_test;
+    var _this = this;
+    wx.request({
+      url: url_tmp + '/club/qryLessReg',
+      data: {
+        club_id: app.globalData.user_id,
+        reg_date: e.detail.value
+        // status: 1 
+      },
+      success(res) {
+        console.log(res.data)
+        _this.setData({
+          clubIncomeData: res.data
+        })
+      }
+    })
+    //获取授课总收 
+    var that = this
+    wx.request({
+      url: url_tmp + '/club/qrySum',
+      data: {
+        club_id: app.globalData.user_id,
+        reg_date: commonData.formatDate(date)
+      },
+      success(res) {
+        console.log(res.data)
+        that.setData({
+          clubIncomeSum: res.data
+        })
+      }
+    })
+  }, 
   /**
    * 生命周期函数--监听页面加载
    */
-	cashClick: function () {
-		var storeRouter = '../../user/cash/cash';
-		var storeTitle = '提现';
-		commonData.routers(storeRouter, storeTitle);
-	},
-	onLoad: function (options) {
-		var url_tmp = commonData.getListConfig().url_test;
-		var _this = this;
-		//获取授课总收
-		wx.request({
-			url: url_tmp + '/club/qryLessReg',
-			data: {
-				club_id: app.globalData.user_id,
-				reg_date: _this.data.date,
-				// status: 1
-			},
-			success(res) {
-				console.log(res.data)
-				_this.setData({
-					clubIncomeData:res.data
-				})
+  cashClick: function () {
+    var storeRouter = '../../user/cash/cash';
+    var storeTitle = '提现';
+    commonData.routers(storeRouter, storeTitle);
+  },
+  onLoad: function (options) {
+    var url_tmp = commonData.getListConfig().url_test;
+    var _this = this;
+    console.log("onload this")
+    //获取授课总收 
+    wx.request({
+      url: url_tmp + '/club/qryLessReg',
+      data: {
+        club_id: app.globalData.user_id,
+        reg_date: _this.data.date,
+        // status: 1 
+      },
+      success(res) {
+        console.log(res.data)
+        _this.setData({
+          clubIncomeData: res.data
+        })
+      }
+    })
+    //获取授课总收 
 
-			}
-		})
-		_this.setData({
-			clubIncomeSum: app.globalData.clubIncomeData
-		})
-	},
+    wx.request({
+      url: url_tmp + '/club/qrySum',
+      data: {
+        club_id: app.globalData.user_id,
+        reg_date: commonData.formatDate(date)
+      },
+      success(res) {
+        console.log(res.data)
+        // app.globalData.clubIncomeData=res.data 
+        _this.setData({
+          clubIncomeSum: res.data
+        })
+      }
+    })
+  }, 
 
   /**
    * 生命周期函数--监听页面初次渲染完成
